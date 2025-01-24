4059841e87953a20ab51d4aea5eac5e8b43d92c7
Alexey Sorokin <asorokin@Alexeys-MacBook-Air.local>
2025-01-23 19:23:14 -0800

    dz nomer 13

--- homework_3_1.py
+++ homework_3_1.py
@@ -20,3 +20,23 @@ def test_headers():
     headers = link.headers
     print(headers)
     assert len(headers.keys()) == 9 and len(headers.values()) == 9, f"None or not all headers are presented in API call"
+
+
+def test_user_agent():
+    user_agents = ['Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+                   'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',
+                   'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+                   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0',
+                   'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+                   ]
+    total = {}
+    for data in user_agents:
+        link = requests.get("https://playground.learnqa.ru/ajax/api/user_agent_check", headers={"User-Agent": data})
+        result = link.json()
+        #print(result)
+        for key, value in result.items():
+            if value == 'Unknown':
+                total.setdefault(data, [])
+                if key not in total[data]:
+                    total[data].append(key)
+    assert not total, f"Some user agent parameters have 'Unknown' data. Please double check the result {total}"
