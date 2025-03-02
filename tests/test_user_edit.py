1552dc9672f648c08bd1c038d69741e97e9e88d3
Alexey Sorokin <asorokin@Alexeys-MacBook-Air.local>
2025-03-02 15:43:55 +0700

    DZ nomer 17

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -5,7 +5,7 @@ from lib.assertions import Assertions
 
 class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
-        # REGISTER
+
         register_data = self.prepare_registration_data()
         response1 = MyRequests.post("/user/", data=register_data)
 
@@ -17,7 +17,6 @@ class TestUserEdit(BaseCase):
         password = register_data['password']
         user_id = self.get_json_value(response1, "id")
 
-        # LOGIN
         login_data = {
             'email': email,
             'password': password
@@ -27,7 +26,6 @@ class TestUserEdit(BaseCase):
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
 
-        # EDIT
         new_name = "Changed Name"
 
         response3 = MyRequests.put(
@@ -39,7 +37,6 @@ class TestUserEdit(BaseCase):
 
         Assertions.assert_code_status(response3, 200)
 
-        # GET
         response4 = MyRequests.get(
             f"/user/{user_id}",
             headers={"x-csrf-token": token},
@@ -52,3 +49,98 @@ class TestUserEdit(BaseCase):
             new_name,
             "Wrong name of the user after edit"
         )
+
+    def test_update_user_not_logged_in(self):
+
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+        Assertions.assert_code_status(response1, 200)
+        user_id = self.get_json_value(response1, "id")
+
+        new_name = "Unauthorized Update"
+        response2 = MyRequests.put(
+            f"/user/{user_id}",
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response2, 401)
+
+    def test_update_another_user(self):
+
+        user_a_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=user_a_data)
+        Assertions.assert_code_status(response1, 200)
+        user_a_id = self.get_json_value(response1, "id")
+
+        user_b_data = self.prepare_registration_data()
+        response2 = MyRequests.post("/user/", data=user_b_data)
+        Assertions.assert_code_status(response2, 200)
+        user_b_id = self.get_json_value(response2, "id")
+
+        login_data = {
+            "email": user_b_data["email"],
+            "password": user_b_data["password"]
+        }
+        response3 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response3, "auth_sid")
+        token = self.get_header(response3, "x-csrf-token")
+
+        new_name = "Updated by Another"
+        response4 = MyRequests.put(
+            f"/user/{user_a_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response4, 403)
+
+    def test_update_email_invalid_format(self):
+
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+        Assertions.assert_code_status(response1, 200)
+        user_id = self.get_json_value(response1, "id")
+
+        login_data = {
+            "email": register_data["email"],
+            "password": register_data["password"]
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        invalid_email = "invalidemail.com"
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"email": invalid_email}
+        )
+
+        Assertions.assert_code_status(response3, 400)
+
+    def test_update_firstname_too_short(self):
+
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+        Assertions.assert_code_status(response1, 200)
+        user_id = self.get_json_value(response1, "id")
+
+        login_data = {
+            "email": register_data["email"],
+            "password": register_data["password"]
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        short_name = "A"
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": short_name}
+        )
+
+        Assertions.assert_code_status(response3, 400)
